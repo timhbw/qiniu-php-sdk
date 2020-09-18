368bc0aae6d9ae22aad0c270823163358a90c648
timhbw <timhbw@gmail.com>
2020-09-18 18:28:04 +0800

    删除无用引用和优化注释

--- src/Qiniu/Storage/UploadManager.php
+++ src/Qiniu/Storage/UploadManager.php
@@ -2,9 +2,6 @@
 namespace Qiniu\Storage;
 
 use Qiniu\Config;
-use Qiniu\Http\HttpClient;
-use Qiniu\Storage\ResumeUploader;
-use Qiniu\Storage\FormUploader;
 
 /**
  * 主要涉及了资源上传接口的实现
@@ -26,13 +23,14 @@ final class UploadManager
     /**
      * 上传二进制流到七牛
      *
-     * @param $upToken    上传凭证
-     * @param $key        上传文件名
-     * @param $data       上传二进制流
-     * @param $params     自定义变量，规格参考
+     * @param string $upToken    上传凭证
+     * @param string $key        上传文件名
+     * @param string $data       上传二进制流
+     * @param string $params     自定义变量，规格参考
      *                    http://developer.qiniu.com/docs/v6/api/overview/up/response/vars.html#xvar
-     * @param $mime       上传数据的mimeType
+     * @param string $mime 上传数据的mimeType
      *
+     * @param string $fname
      * @return array    包含已上传文件的信息，类似：
      *                                              [
      *                                                  "hash" => "<Hash string>",
@@ -64,13 +62,13 @@ final class UploadManager
     /**
      * 上传文件到七牛
      *
-     * @param $upToken    上传凭证
-     * @param $key        上传文件名
-     * @param $filePath   上传文件的路径
-     * @param $params     自定义变量，规格参考
+     * @param string $upToken    上传凭证
+     * @param string $key        上传文件名
+     * @param string $filePath   上传文件的路径
+     * @param string $params     自定义变量，规格参考
      *                    http://developer.qiniu.com/docs/v6/api/overview/up/response/vars.html#xvar
-     * @param $mime       上传数据的mimeType
-     * @param $checkCrc   是否校验crc32
+     * @param string $mime       上传数据的mimeType
+     * @param bool  $checkCrc   是否校验crc32
      *
      * @return array    包含已上传文件的信息，类似：
      *                                              [
